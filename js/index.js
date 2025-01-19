7a5e9b1502bc49e61635701fdf2f3ba48cc0cb28
Jessica Plemmons <jessica.plemmons@hotmail.com>
2025-01-19 03:20:48 -0500

    onto part two - confused tho :(

--- js/index.js
+++ js/index.js
@@ -6,10 +6,30 @@ if (num % 2 === 0) {
     evenOrOddElement.textContent = ("Even");
 }   else {
     evenOrOddElement.textContent = ("Odd");
-}
-*/
+
+}   
 
 let apple = [1, 2, 3, 4, 5];
  for (let i = 0; i < apple.length; i++) {
      console.log(apple[i]);
  }
+*/
+
+/*
+let i = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 5);
+ 
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+*/
