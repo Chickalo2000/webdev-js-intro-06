a3774312775b33422ee2369e7e9aaab78297039a
Jessica Plemmons <jessica.plemmons@hotmail.com>
2025-01-19 03:49:18 -0500

    Unsure what im doing

--- js/assignment.js
+++ js/assignment.js
@@ -9,7 +9,12 @@ function evenOrOdd() {
     const num = 3;
     // Write the logic to decide if the variable "num" is even or odd
     // and set the element's value the string "Even" or "Odd" accordingly
-   
+   if (num % 2 === 0) {
+       evenOrOddElement.value = "Even";
+   } else {
+       evenOrOddElement.value = "Odd";
+   }
+  
 }
 
 function sumTheNumbers() {
@@ -17,7 +22,10 @@ function sumTheNumbers() {
     // Write the logic to sum the numbers 1 through 10
     // using a for loop. Check the expected output
     // on the assignment page
-
+    for (let i = 1; i <= 10; i++) {
+        sum += i;
+    }
+    sumTheNumbersElement.value = sum;
 }
 
 function createNumberArray() {
@@ -26,7 +34,10 @@ function createNumberArray() {
     // Write the logic that loops 10 times and adds the value
     // to numberArray each iteration. Check the expected output
     // on the assignment page
-
+    for (let i = 1; i <= 10; i++) {
+        numberArray.push(i);
+    }
+    createNumberArrayElement.value = numberArray.join(", ");
 }
 
 function render() {
